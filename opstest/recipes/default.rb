b6017e6bdb2206fe878c8dfde27f386ad99d823a
opsworksalem <u.askhat@alem.kz>
2015-08-14 16:10:40 +0600

    Update default.rb

--- opstest/recipes/default.rb
+++ opstest/recipes/default.rb
@@ -2,19 +2,19 @@ chef_gem 'rufus-scheduler'
 require 'rufus-scheduler'
 scheduler = Rufus::Scheduler.new
 
-scheduler.every '1m' do
-	data_dir = value_for_platform(
-	  "centos" => { "default" => "/srv/www/shared" },
-	  "ubuntu" => { "default" => "/srv/www/data" },
-	  "default" => "/srv/www/config"
-	)
-	
-	directory data_dir do
-	  mode 0755
-	  owner 'root'
-	  group 'root'
-	  recursive true
-	  action :create
-	end
+
+data_dir = value_for_platform(
+  "centos" => { "default" => "/srv/www/shared" },
+  "ubuntu" => { "default" => "/srv/www/data" },
+  "default" => "/srv/www/config"
+)
+
+directory data_dir do
+  mode 0755
+  owner 'root'
+  group 'root'
+  recursive true
+  action :create
 end
+
 

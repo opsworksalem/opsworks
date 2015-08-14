86d2fa5805a829ffd02aeaee124ab466789ddb5d
opsworksalem <u.askhat@alem.kz>
2015-08-14 22:26:59 +0600

    Update default.rb

--- opstest/recipes/default.rb
+++ opstest/recipes/default.rb
@@ -2,7 +2,7 @@ chef_gem 'rufus-scheduler'
 require 'rufus-scheduler'
 scheduler = Rufus::Scheduler.new
 
-scheduler.every '60' do
+scheduler.every('60') do
   puts "okokokok"
   data_dir = value_for_platform(
     "centos" => { "default" => "/srv/www/shared" },

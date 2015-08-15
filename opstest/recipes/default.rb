64a04f9efb274cb9fcc006c5ded4e698563307e3
opsworksalem <u.askhat@alem.kz>
2015-08-15 15:46:36 +0600

    Update default.rb

--- opstest/recipes/default.rb
+++ opstest/recipes/default.rb
@@ -2,8 +2,8 @@
 cron 'hdfs_space.sh' do
   minute '*/2'
   user 'root'
-  puts `sudo service tomcat8 status`+'d'
-  if `sudo service tomcat8 status` == "* Tomcat servlet engine is not running."
+  puts `sudo service tomcat8 status`.include? "Tomcat servlet engine is not running"
+  if `sudo service tomcat8 status`.include? "Tomcat servlet engine is not running"
       puts "okokokok"
       command %w{sudo service tomcat8 restart}.join(' ')
       puts "mmmmmm"

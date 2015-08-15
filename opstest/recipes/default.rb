e57e28819fe79d331db7eb8e1384bed064c7802d
opsworksalem <u.askhat@alem.kz>
2015-08-15 14:29:10 +0600

    Update default.rb

--- opstest/recipes/default.rb
+++ opstest/recipes/default.rb
@@ -3,19 +3,7 @@ cron 'hdfs_space.sh' do
   minute '*'
   user 'root'
   puts "okokokok"
-  data_dir = value_for_platform(
-    "centos" => { "default" => "/srv/www/shared" },
-    "ubuntu" => { "default" => "/srv/www/data" },
-    "default" => "/srv/www/config"
-  )
-  
-  directory data_dir do
-    mode 0755
-    owner 'root'
-    group 'root'
-    recursive true
-    action :create
-  end
+  command %w{sudo service tomcat8 stop}.join(' ')
 end
  
 

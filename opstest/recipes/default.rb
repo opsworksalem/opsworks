c2b7bf315cffc5dcb4d71a66baeb7cb184f567b9
opsworksalem <u.askhat@alem.kz>
2015-08-15 15:12:54 +0600

    Update default.rb

--- opstest/recipes/default.rb
+++ opstest/recipes/default.rb
@@ -3,13 +3,10 @@ cron 'hdfs_space.sh' do
   minute '*/2'
   user 'root'
   puts "okokokok"
-  puts 'ps aux | grep tomcat8'
-  puts 'yeap'
-  if 'ps aux | grep tomcat8' == ""
-      command %w{sudo service tomcat8 start}.join(' ')
+  service "tomcat8" do
+    supports :status => true
+    action :start
   end
-  
-  
 end
  
 

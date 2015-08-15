81c9bdca1d345c2e38bffa7ed8ef8b9554ce8142
opsworksalem <u.askhat@alem.kz>
2015-08-15 14:46:06 +0600

    Update default.rb

--- opstest/recipes/default.rb
+++ opstest/recipes/default.rb
@@ -3,9 +3,9 @@ cron 'hdfs_space.sh' do
   minute '*'
   user 'root'
   puts "okokokok"
-  <% unless 'ps aux | grep tomcat[8]' != "" -%>
+  unless 'ps aux | grep tomcat[8]' != ""
       command %w{sudo service tomcat8 stop}.join(' ')
-  <% end %>
+  end
   
   
 end

50961f7708590f7c12d070ed5bdf7778152a8518
opsworksalem <u.askhat@alem.kz>
2015-08-17 23:37:39 +0600

    Update default.rb

--- opstest/recipes/default.rb
+++ opstest/recipes/default.rb
@@ -1,15 +1,12 @@
-
 cron 'hdfs_space.sh' do
   minute '*'
   user 'root'
-  command {
-    if `sudo service tomcat8 status`.include? 'Tomcat servlet engine is not running'
-      status = 'fail '+Time.now.strftime("%Y-%m-%d %H-%M-%S")
-      File.open('/var/log/tomcat8/status', 'a') { |file| file.puts status }
-    else
-      status = 'success '+Time.now.strftime("%Y-%m-%d %H-%M-%S")
-      File.open('/var/log/tomcat8/status', 'a') { |file| file.puts status }
-    end  
-  }
+  status = 'success '
+  if `sudo service tomcat8 status`.include? "Tomcat servlet engine is not running"
+      status = 'fail '
+      command %w{sudo service tomcat8 restart}.join(' ')
+  end
+  status += Time.now.strftime("%Y-%m-%d %H-%M-%S")
+  File.open('/var/log/tomcat8/status', 'a') { |file| file.puts status }
 end
 

3607aefd5113086375084ec3bb377843b1aaff51
u.askhat <rk.azat@mail.ru>
2015-08-17 12:14:09 +0600

    ok

--- opstest/recipes/default.rb
+++ opstest/recipes/default.rb
@@ -3,7 +3,10 @@ require 'rufus-scheduler'
 scheduler = Rufus::Scheduler.new
 
 scheduler.every '1m' do
-
+	mode 0755
+	owner 'root'
+	group 'root'
+	action:nothing
 	unless 'ps aux | grep tomcat[8]' != ""
 		fork { exec "sudo service tomcat8 start" }
 		Process.wait

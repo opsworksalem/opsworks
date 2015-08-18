6f6ad60810902c5afe0b7d96d1a3c24371b8b82e
opsworksalem <u.askhat@alem.kz>
2015-08-18 11:06:20 +0600

    Update default.rb

--- opstest/recipes/default.rb
+++ opstest/recipes/default.rb
@@ -1,21 +1,17 @@
-cron 'hdfs_space.sh' do
-  minute '*'
-  user 'root'
-  command %w{sudo service tomcat8 restart}.join(' ')
-  only_if {
-          status = 'fail '
-          status += Time.now.strftime("D%Y%m%dT%H%M%S")
-          File.open('/var/log/tomcat8/status', 'a') { |file| file.puts status }
-          `sudo service tomcat8 status`.include? "Tomcat servlet engine is not running"
-  }
-  not_if {
-          status = 'success '
-          status += Time.now.strftime("D%Y%m%dT%H%M%S")
-          File.open('/var/log/tomcat8/status', 'a') { |file| file.puts status }
-          `sudo service tomcat8 status`.include? "Tomcat servlet engine is running with pid"
-    
-  }
-  
-  
+gem_package 'rufus-scheduler' do
+  version '3.1.3'
+end
+require 'rufus-scheduler'
+scheduler = Rufus::Scheduler.new
+
+scheduler.every '1m' do
+  puts "okokokok"
+  if `sudo service tomcat8 status`.include? "Tomcat servlet engine is not running"
+    status = 'fail '
+  else
+    status = 'success '
+  end
+  status += Time.now.strftime("D%Y%m%dT%H%M%S")
+  File.open('/var/log/tomcat8/status', 'a') { |file| file.puts status }
 end
 

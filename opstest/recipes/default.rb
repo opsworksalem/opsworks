405b3cb367c610986e079d925df5c6bd0a5a8cfa
opsworksalem <u.askhat@alem.kz>
2015-08-19 18:45:17 +0600

    Update default.rb

--- opstest/recipes/default.rb
+++ opstest/recipes/default.rb
@@ -2,7 +2,7 @@ chef_gem 'rufus-scheduler'
 require 'rufus-scheduler'
 scheduler = Rufus::Scheduler.new
 
-scheduler.every '1m' do
+scheduler.repeat '1m' do
    if `sudo service tomcat8 status`.include? "Tomcat servlet engine is not running"
      status = 'fail '
      fork { exec "sudo service tomcat8 restart" }
@@ -12,6 +12,5 @@ scheduler.every '1m' do
    status += Time.now.strftime("D%Y-%m-%dT%H:%M:%S ")
    status += `hostname`
    File.open('/var/log/tomcat8/status', 'a') { |file| file.puts status }
-   scheduler.join
 end
 

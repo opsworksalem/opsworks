a4b16028bde498443f5cbf1c236183c9feec101b
opsworksalem <u.askhat@alem.kz>
2015-08-19 21:40:23 +0600

    Update cronjob.rb

--- opstest/recipes/cronjob.rb
+++ opstest/recipes/cronjob.rb
@@ -1,6 +1,1 @@
-cron "status" do
-  minute "*"
-  command if ((`ps aux | grep tomcat[8]` == "")); then echo "fail" >> /var/log/tomcat8/status; else echo "success" >> /var/log/tomcat8/status;
-    fi
-end
 

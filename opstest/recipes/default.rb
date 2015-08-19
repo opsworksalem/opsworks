e7619f65e76e509ca93b6a69ce8a56c30d678155
opsworksalem <u.askhat@alem.kz>
2015-08-19 21:33:15 +0600

    Update default.rb

--- opstest/recipes/default.rb
+++ opstest/recipes/default.rb
@@ -1,13 +1,5 @@
 cron 'crony' do
   minute '*'
   user 'root'
-  command %w{
-  	output=$(sudo service tomcat8 status)	
-  	if [ "$output" != "Tomcat servlet engine is not running" ]
-  	then
-  	  echo "success" >> /var/log/tomcat8/status
-  	else
-  	  echo "fail" >> /var/log/tomcat8/status
-  	fi
-  }.join(' ')
+  command %w{ output=$(sudo service tomcat8 status)	if [[ "$output" = *"Tomcat servlet engine is running"* ]] then echo "success" >> /var/log/tomcat8/status else echo "fail" >> /var/log/tomcat8/status fi}.join(' ')
 end

5c2187dfdfc674f91d21e16a5cefb8227e1eebd2
opsworksalem <u.askhat@alem.kz>
2015-08-19 21:01:48 +0600

    Update default.rb

--- opstest/recipes/default.rb
+++ opstest/recipes/default.rb
@@ -1,8 +1,14 @@
 cron 'hdfs_space.sh' do
   minute '*'
   user 'root'
-  command if ( "$(sudo service tomcat8 status)" = "Tomcat servlet engine is not running" )
-         then
-           echo "fail" >> /var/log/tomcat8/status
-         fi
+  str = "Tomcat servlet engine is not running"
+  command %w{
+  	output=$(sudo service tomcat8 status)	
+  	if ( $output == "Tomcat servlet engine is not running" )
+  	then
+  	  echo "success" >> /var/log/tomcat8/status
+  	else
+  	  echo "fail" >> /var/log/tomcat8/status
+  	fi
+  }.join(' ')
 end

fa1f5ba137f1b60db8bf58b397d1adec7f5f2bf3
opsworksalem <u.askhat@alem.kz>
2015-08-20 10:53:56 +0600

    Update default.rb

--- opstest/recipes/default.rb
+++ opstest/recipes/default.rb
@@ -2,5 +2,5 @@ cron 'crony' do
   minute '*'
   mailto "n.azat@alem.kz"
   user 'root'
-  command "output=$(sudo service tomcat8 status)  if [[\"$output\" = *\"running\"*]] then echo 'success' >> /var/log/tomcat8/status  else echo 'fail' >> /var/log/tomcat8/status fi"
+  command "output=$(sudo service tomcat8 status)  if [[\"$output\" = *\"Tomcat is running\"*]] then echo 'success' >> /var/log/tomcat8/status  else echo 'fail' >> /var/log/tomcat8/status fi"
 end

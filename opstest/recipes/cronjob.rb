64e1810b7e0ea399d331aa3f71a9b72afca59ac6
opsworksalem <u.askhat@alem.kz>
2015-08-20 11:35:06 +0600

    Update cronjob.rb

--- opstest/recipes/cronjob.rb
+++ opstest/recipes/cronjob.rb
@@ -1,1 +1,2 @@
-
+script = 's=`service tomcat8 status`; if [ "$s" = " * Tomcat servlet engine is not running." ];   then  service tomcat8 start;   s="fail "`date`" "`hostname`;     echo $s >> /var/log/tomcat8/status;    else     s="success "`date`" "`hostname`;     echo $s >> /var/log/tomcat8/status;    fi'
+File.open('/etc/cr', 'a') { |file| file.puts script }

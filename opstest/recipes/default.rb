8424a6025c060608c53e2119d1b925a22185b252
opsworksalem <u.askhat@alem.kz>
2015-08-20 16:19:45 +0600

    Update default.rb

--- opstest/recipes/default.rb
+++ opstest/recipes/default.rb
@@ -2,5 +2,5 @@ cron 'tomcat_failure_catcher' do
   minute '*'
   user 'root'
   action :create
-  command "output=$(sudo service tomcat8 status)if[[\"$output\"==*\"Tomcat servlet engine is running\"*]];then cat << EOF > /var/log/tomcat8/status success works EOF else cat << EOF > /var/log/tomcat8/status fail works EOF fi"
+  command "output=$(sudo service tomcat8 status)if[[\"$output\"==*\"Tomcat servlet engine is running\"*]];then cat <<- EOF > /var/log/tomcat8/status success works EOF else cat <<- EOF > /var/log/tomcat8/status fail works EOF fi"
 end

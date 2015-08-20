c0923ca4933dffc5ab3129cf2bf4c1cd2198870e
opsworksalem <u.askhat@alem.kz>
2015-08-20 12:27:46 +0600

    Update default.rb

--- opstest/recipes/default.rb
+++ opstest/recipes/default.rb
@@ -2,5 +2,5 @@ cron 'tomcat_failure_catcher' do
   minute '*'
   user 'root'
   action :create
-  command "output=$(sudo service tomcat8 status) echo 'success' >> /var/log/tomcat8/status "
+  command "output=$(sudo service tomcat8 status) echo 'success'>>/var/log/tomcat8/status"
 end
